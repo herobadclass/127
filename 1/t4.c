9c53baa965304582020d6b8bb19cac158394dc9a
jzfeng <jzfeng@sfu.ca>
2018-05-22 13:38:16 -0700

    t4 submission

---
+++ 1/t4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main(void)
+{
+	float i = 0;
+	printf("Enter an float point value: ");
+	scanf("%f", &i );
+	if(i == 0)
+	{
+		printf("scanf error: (%d)\n", (int)i);
+	}
+	else
+	{
+		printf("%d %d %d\n", (int)floor(i), (int)round(i), (int)ceil(i));
+	}
+	return 0;
+}
