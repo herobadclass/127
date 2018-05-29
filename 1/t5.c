dee2e9dc9df7779abcfa805d2ccaddf84a37f97c
jzfeng <jzfeng@sfu.ca>
2018-05-29 12:40:33 -0700

    t5 submission

---
+++ 1/t5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+ 
+int main(void)
+{
+	float i = 0;
+	printf("Enter an float point value: ");
+	while(scanf("%f", &i ) != EOF)
+	{
+		printf("%d %d %d\n", (int)floor(i), (int)round(i), (int)ceil(i));
+	}
+	if(scanf("%f", &i ) == EOF)
+	{
+		printf("Done.\n");
+	}
+	return 0;
+}
