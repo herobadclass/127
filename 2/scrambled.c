75f07c33195cc22a03f9b7540cd5917b62bc0986
jzfeng <jzfeng@sfu.ca>
2018-06-05 13:44:35 -0700

    scrambled is working

---
+++ 2/scrambled.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int scrambled( unsigned int a[], unsigned int b[], unsigned int len )
+{
+	int rangeA[100];
+	int rangeB[100];
+	for(int i; i < len; i++)
+	{
+		rangeA[a[i]] = rangeA[a[i]] + 1;
+		rangeB[b[i]] = rangeB[b[i]] + 1;
+	}
+	for(int i; i < 100; i++)
+	{
+		if(rangeA[i] != rangeB[i])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
